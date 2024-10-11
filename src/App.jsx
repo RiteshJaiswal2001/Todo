90bf61293a2ce09d75a8983fce2079b31d91d1f1
Ritesh Kumar <rkjais10022001@gmail.com>
2024-10-12 04:40:45 +0530

    add and remove work is done

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import AddTodo from './components/AddTodo'
+import Todos from './components/Todos'
+
+function App() {
+  
+
+  return (
+    <>
+   <h1 className='font-extrabold text-2xl'>Redux toolkit concept</h1>
+   <AddTodo/>
+   <Todos/>
+   
+    </>
+  )
+}
+
+export default App
